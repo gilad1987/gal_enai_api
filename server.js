93c4a85f2c0f3b481d22c4feb2498764251c0156
Gilad takoni <gilad1987@gmail.com>
2018-07-09 13:28:03 +0300

    .

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ async function getList(parent) {
 app.get('/', async (req, res) => {
     const dirTree = require('directory-tree');
     const filteredTree = dirTree('./ginzburg_books');
-    let html = '<style>#tree{min-width: 30%;} .name{cursor: pointer;} body{direction: rtl} #content{    flex: 1 1 auto;} .wrapper{ display: flex; }</style><div class="wrapper"><div id="tree">';
+    let html = '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body>';
+    html += '<style>#tree{min-width: 30%;} .name{cursor: pointer;} body{direction: rtl} #content{    flex: 1 1 auto;} .wrapper{ display: flex; }</style><div class="wrapper"><div id="tree">';
     try {
         html += await getList(filteredTree);
 
@@ -58,8 +59,8 @@ app.get('/', async (req, res) => {
 app.get('/gal_enai', async (req, res) => {
     const dirTree = require('directory-tree');
     const filteredTree = dirTree('./ginzburg_books');
-    let html = '<!DOCTYPE html><html><head><meta http-equiv="Content-type" content="text/html; charset=utf-8"></head><body>';
-    html = '<style>#tree{min-width: 30%;} .name{cursor: pointer;} body{direction: rtl} #content{    flex: 1 1 auto;} .wrapper{ display: flex; }</style><div class="wrapper"><div id="tree">';
+    let html = '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body>';
+    html += '<style>#tree{min-width: 30%;} .name{cursor: pointer;} body{direction: rtl} #content{    flex: 1 1 auto;} .wrapper{ display: flex; }</style><div class="wrapper"><div id="tree">';
     try {
         html += await getList(filteredTree);
 
